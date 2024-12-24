c344def63f096b8503eeeb784b05e2827c2d6b56
Alexey Maryshenkov <alekseycrosso@gmail.com>
2024-12-25 00:07:46 +0300

    some fixes

--- main.rb
+++ main.rb
@@ -116,48 +116,51 @@ class Main
     search_suit = ''
     search_rank = ''
 
-    puts 'Укажите масть: '
+
     puts '1 - ❤️'
     puts '2 - ♦️'
     puts '3 - ♣️'
     puts '4 - ♠️'
-    suit = gets.to_i
+    print 'Укажите масть: '
 
     loop do
+      suit = gets.to_i
+
       if suit < 1 || suit > 4
-        puts 'Ошибка! Введите корректное значение'
+        print 'Ошибка! Введите корректное значение: '
       else
         search_suit = SUITS_HASH[suit]
         break
       end
     end
 
-    puts 'Укажите значение: '
-    puts '1 => 2'
-    puts '2 => 3'
-    puts '3 => 4'
-    puts '4 => 5'
-    puts '5 => 6'
-    puts '6 => 7'
-    puts '7 => 8'
-    puts '8 => 9'
-    puts '9 => 10'
-    puts '10 => Jack'
-    puts '11 => Queen'
-    puts '12 => King'
-    puts '13 => Ace'
-
-    rank = gets.to_i
+    puts '1 - 2'
+    puts '2 - 3'
+    puts '3 - 4'
+    puts '4 - 5'
+    puts '5 - 6'
+    puts '6 - 7'
+    puts '7 - 8'
+    puts '8 - 9'
+    puts '9 - 10'
+    puts '10 - Jack'
+    puts '11 - Queen'
+    puts '12 - King'
+    puts '13 - Ace'
+    print 'Укажите значение: '
 
     loop do
+      rank = gets.to_i
+
       if rank < 1 || rank > 13
-        puts 'Ошибка! Введите корректное значение'
+        print 'Ошибка! Введите корректное значение: '
       else
         search_rank = RANKS_HASH[rank]
         break
       end
     end
 
+    search_card = "#{search_rank}#{search_suit}"
     search_value = SUITS_VALUES[search_suit] * RANKS.size + RANKS_VALUES[search_rank]
 
     low = 0
@@ -173,13 +176,13 @@ class Main
       elsif deck[mid].value < search_value
         low = mid + 1
       else
-        puts "Такая карта есть в коллоде!"
+        puts "#{search_card} - Такая карта есть в коллоде!"
         found = true
         break
       end
     end
 
-    puts 'Такой карты нет в коллоде!' unless found
+    puts "#{search_card} - Такой карты нет в коллоде!" unless found
   end
 end
 
